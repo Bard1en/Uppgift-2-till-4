ad5d05ac9930225ce3519b4842c31c8f8b43dd60
Bard1en <greger.grym@gmail.com>
2024-04-10 09:59:11 +0200

    Add project files.

---
+++ ConsoleApp35/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class person
+    {
+        public string? förnamn;
+        public string? efternamn;
+        public int år;
+        public int månad;
+        private int längd;
+        private int vikt;
+        public int mymetod
+        {
+            get { return längd; }
+            set { längd = value; }
+        }
+        public int mymetod2
+        {
+            get { return vikt; }
+            set { vikt = value; }
+        }
+        public void Mymetod3()
+        {
+            person p = new person();
+            int bmivikt = vikt;
+            int dubbellängd = längd * längd;
+            int bmi = 0;
+            bmivikt / dubbellängd = bmi;
+            Console.WriteLine("Din bmi är: " + bmi);
+        }
+        public void metod1()
+        {
+            person p = new person();
+            Console.WriteLine("Skriv in ditt förnamn:");
+            p.förnamn = Console.ReadLine();
+            Console.WriteLine("Skriv in ditt efternamn:");
+            p.efternamn = Console.ReadLine();
+            Console.WriteLine(p.förnamn + " " + p.efternamn);
+            
+        }
+        public void metod3()
+        {
+            person p = new person();
+            Console.WriteLine("Vilket år föddes du?");
+            p.år = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken månad föddes du? (1-12)");
+            p.månad = int.Parse(Console.ReadLine());
+            DateTime datetime = DateTime.Now;
+            if (datetime.Year - p.år >= 18 && datetime.Month - p.månad >= 0)
+            {
+                Console.WriteLine("myndig");
+            }
+            else
+            {
+                Console.WriteLine("bäbis");
+            }
+            
+
+        }
+      
+
+     
+
+    }
+}
---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            person p = new person();
+            p.metod1();
+            p.metod3();
+            Console.WriteLine("Hur lång är du?");
+            p.mymetod = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket väger du?");
+            p.mymetod2 = int.Parse(Console.ReadLine());
+            p.Mymetod3();
+        }
+    }
+}
